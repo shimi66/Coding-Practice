483b3ce17aef15b4a273a18a249f251f9c33c6b2
Erik Voss <erik66voss@gmail.com>
2025-11-14 21:02:15 -0800

    syntax fix

--- LeetCode/Easy/E_217.py
+++ LeetCode/Easy/E_217.py
@@ -9,6 +9,9 @@ Initial Thoughts
     - topics
         - sets/arrays
 '''
+
+from typing import List
+
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
         return len(set(nums)) != len(nums)
